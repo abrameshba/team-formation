b3b56bc38b18a27ce80e1bfbb81b83f104303d60
A B Ramesh <abrameshba@gmail.com>
2021-04-04 15:50:14 +0530

    updated bash script to remove intermediate files

--- clean_data.sh
+++ clean_data.sh
@@ -5,7 +5,7 @@ year=2020
 
 # remove existing files 
 rm -v ./"dblp-"$year/*.txt
-rm -v ./"dblp-"$year/*.xml
+
 
 
 # extracting the .xml.gz file
@@ -13,16 +13,19 @@ gunzip -k ./"dblp-"$year/"dblp-"$year"-04-01.xml.gz"
 
 # rearrange with newline for each tag closing
 sed "s/></>\n</g" ./dblp-$year/dblp-$year-04-01.xml  > ./"dblp-"$year/t1.txt
+rm -v ./dblp-$year/dblp-$year-04-01.xml
 
 # remove non-relevant tags 
 grep -v "<note\|<cite\|<url\|<crossref\|<pages\|<volume\|<number\|<ee\|<cdrom\|<month\|<series\|<editor\|<publisher\|<isbn" ./"dblp-"$year/t1.txt > ./"dblp-"$year/t2.txt
 rm -v ./"dblp-"$year/t1.txt
+
 # extract all articles with () information
 sed -n '/<article/,/<\/article>/p' ./"dblp-"$year/t2.txt | tr -d "\n" | sed 's/<article/\n<article/g' | sed 's/<\/article>/<\/article>\n/g' > ./"dblp-"$year/at3.txt
 
 # remove attributes and values (mdate,key,pubtype of article and orcid of author)
 sed 's/\smdate=\"[0-9-]*\"//g' ./"dblp-"$year/at3.txt	| sed 's/\skey=\"[a-z\/A-Z0-9-]*\"//g' | sed 's/\spubltype=\"[a-z\/A-A ]*\"//g' | sed 's/\sorcid=\"[0-9X\-]*\"//g' > ./"dblp-"$year/at4.txt
 rm -v ./"dblp-"$year/at3.txt
+
 # remove invalid entries  and tags (<article>, </article>)
 grep -v 'publtype=\"withdrawn\"' ./"dblp-"$year/at4.txt | sed 's/<\/author>[[:space:]]*<author>/:/g' | sed '/^[[:space:]]*$/d' > ./"dblp-"$year/articles.txt
 rm -v ./"dblp-"$year/at4.txt
@@ -30,9 +33,11 @@ rm -v ./"dblp-"$year/at4.txt
 # extract all conferences with () information
 sed -n '/<inproceedings/,/<\/inproceedings>/p' ./"dblp-"$year/t2.txt | tr -d "\n" | sed 's/<inproceedings/\n<inproceedings/g' | sed 's/<\/inproceedings>/<\/inproceedings>\n/g' > ./"dblp-"$year/it3.txt
 rm -v ./"dblp-"$year/t2.txt
+
 # remove attributes and values (mdate,key,pubtype of article and orcid of author)
 sed 's/\smdate=\"[0-9-]*\"//g' ./"dblp-"$year/it3.txt	| sed 's/\skey=\"[a-z\/A-Z0-9-]*\"//g' | sed 's/\spubltype=\"[a-z\/A-A ]*\"//g' | sed 's/\sorcid=\"[0-9X\-]*\"//g' > ./"dblp-"$year/it4.txt
 rm -v ./"dblp-"$year/it3.txt
+
 # remove invalid entries  and tags (<article>, </article>)
 grep -v 'publtype=\"withdrawn\"' ./"dblp-"$year/it4.txt | sed 's/<\/author>[[:space:]]*<author>/:/g' | sed '/^[[:space:]]*$/d' > ./"dblp-"$year/inproceedings.txt
 rm -v ./"dblp-"$year/it4.txt
