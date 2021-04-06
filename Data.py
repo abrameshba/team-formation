03cacd067363be6291c19c8a4df6a1f26280c650
A B Ramesh <abrameshba@gmail.com>
2021-04-06 20:15:37 +0530

    utilities first commit

--- Data.py
+++ Data.py
@@ -4,13 +4,13 @@ from tqdm import tqdm
 import mmap
 
 
-class PublicationRecord:
-
-    def __init__(self, year, title, authors, journal):
-        self.title = title
-        self.authors = list()
-        self.year = year
-        self.journal = journal
+# class PublicationRecord:
+#
+#     def __init__(self, year, title, authors, journal):
+#         self.title = title
+#         self.authors = list()
+#         self.year = year
+#         self.journal = journal
 
 
 # ref : https://blog.nelsonliu.me/2016/07/30/progress-bars-for-python-file-reading-with-tqdm/
@@ -159,14 +159,160 @@ def make_reocrds_file(t_name):
     nx.write_gml(largest_cc, "../dblp-2015/j" + t_name + ".gml")
 
 
+def make_community_reocrds_file(t_name):
+    #     publicationsRecord = publicationsRecord()
+    authors_dict = dict()  # author name is key and id are values
+    authors_id_dict = dict()  # author id is key and name are values
+    authors_publications_dict = dict()
+    authors_skills_dict = dict()
+    collaborations_dict = dict()
+    import utilities
+    import networkx as nx
+    graph = nx.read_gml("../dblp-2015/jdblp.gml")
+    dblp_authors_dict = dict()
+    dblp_authors_id_dict = dict()
+    for node in graph.nodes():
+        dblp_authors_id_dict[node] = graph.nodes[node]["nname"]
+        dblp_authors_dict[graph.nodes[node]["nname"]] = node
+    with open("../dblp-2015/" + t_name + ".txt") as file:
+        n_lines = get_num_lines("../dblp-2015/" + t_name + ".txt")
+        open("../dblp-2015/" + t_name + "-rec.txt", "w").close()
+        for line in tqdm(file, total=n_lines):
+            year = ""
+            if "<year>" in line:
+                year = line[line.index("<year>") + len("<year>"):line.index("</year>")]
+            else:
+                pass
+            title = ""
+            if "<title>" in line:
+                title = line[line.index("<title>") + len("<title>"):line.index("</title>") - 1]
+            else:
+                pass
+            authors = list()
+            if "<author>" in line:
+                authors = line[line.index("<author>") + len("<author>"):line.index("</author>")].split(":")
+                for author in authors:
+                    if author in authors_dict.values():
+                        if title in authors_publications_dict[authors_id_dict[author]]:
+                            pass
+                        else:
+                            authors_publications_dict[authors_id_dict[author]].append(title)
+                        pass
+                    else:
+                        if author in dblp_authors_dict.keys():
+                            if author not in authors_dict:
+                                authors_dict[author] = dblp_authors_dict[author]
+                            if authors_dict[author] not in authors_id_dict:
+                                authors_id_dict[authors_dict[author]] = author
+                            if dblp_authors_dict[author] not in authors_publications_dict:
+                                authors_publications_dict[dblp_authors_dict[author]] = []
+                                authors_publications_dict[dblp_authors_dict[author]].append(title)
+                if len(authors) > 1:
+                    for collaborator_1 in authors:
+                        for collaborator_2 in authors:
+                            if collaborator_1 != collaborator_2:
+                                if str(authors_id_dict[collaborator_1]) + ":" + str(
+                                        authors_id_dict[collaborator_2]) in collaborations_dict:
+                                    if title not in collaborations_dict[
+                                        str(authors_id_dict[collaborator_1]) + ":" + str(
+                                            authors_id_dict[collaborator_2])]:
+                                        collaborations_dict[str(authors_id_dict[collaborator_1]) + ":" + str(
+                                            authors_id_dict[collaborator_2])].append(title)
+                                    else:
+                                        pass
+                                elif str(authors_id_dict[collaborator_2]) + ":" + str(
+                                        authors_id_dict[collaborator_1]) in collaborations_dict:
+                                    if title not in collaborations_dict[
+                                        str(authors_id_dict[collaborator_2]) + ":" + str(
+                                            authors_id_dict[collaborator_1])]:
+                                        collaborations_dict[str(authors_id_dict[collaborator_2]) + ":" + str(
+                                            authors_id_dict[collaborator_1])].append(title)
+                                    else:
+                                        pass
+                                else:
+                                    collaborations_dict[str(authors_id_dict[collaborator_2]) + ":" + str(
+                                        authors_id_dict[collaborator_1])] = list()
+                                    collaborations_dict[str(authors_id_dict[collaborator_2]) + ":" + str(
+                                        authors_id_dict[collaborator_1])].append(title)
+            else:
+                pass
+            journal = ""
+            if "<journal>" in line:
+                journal = line[line.index("<journal>") + len("<journal>"):line.index("</journal>")]
+            else:
+                pass
+            if "<booktitle>" in line:
+                journal = line[line.index("<booktitle>") + len("<booktitle>"):line.index("</booktitle>")]
+            else:
+                pass
+            if len(year) > 0 and int(year) > 1999 and len(authors) > 0 and len(title) > 0 and len(journal) > 0:
+                record = year
+                record += "\t" + journal
+                record += "\t" + ":".join(authors)
+                record += "\t" + title + "\n"
+                open("../dblp-2015/" + t_name + "-rec.txt", "a").write(record)
+    import os
+    open("../dblp-2015/" + t_name + "-records.txt", "w").close()
+    os.system('sort ../dblp-2015/' + t_name + '-rec.txt > ../dblp-2015/' + t_name + '-records.txt')
+    os.system('rm -v ../dblp-2015/' + t_name + '-rec.txt')
+    open("../dblp-2015/" + t_name + "-authors.txt", "w").close()
+    for aid in authors_dict.keys():
+        open("../dblp-2015/" + t_name + "-authors.txt", "a").write(str(aid) + "\t" + authors_dict[aid] + "\n")
+    open("../dblp-2015/" + t_name + "-authors-publications.txt", "w").close()
+    open("../dblp-2015/" + t_name + "-authors-skills.txt", "w").close()
+    skills_set = set()
+    for aid in authors_publications_dict.keys():
+        open("../dblp-2015/" + t_name + "-authors-publications.txt", "a").write(
+            str(aid) + "\t" + " ".join(authors_publications_dict[aid]) + "\n")
+        skills = utilities.get_skills(" ".join(authors_publications_dict[aid]))
+        for skl in skills:
+            skills_set.add(skl)
+    skills_dict = dict()
+    skills_id_dict = dict()
+    skill_id = 1
+    for skill in sorted(skills_set):
+        skills_dict[skill] = skill_id
+        skills_id_dict[skill_id] = skill
+        skill_id += 1
+    open("../dblp-2015/" + t_name + "-skills.txt", "w").close()
+    for sid in skills_id_dict.keys():
+        open("../dblp-2015/" + t_name + "-skills.txt", "a").write(str(sid) + "\t" + str(skills_id_dict[sid]) + "\n")
+    for aid in authors_publications_dict.keys():
+        expert_skills = utilities.get_skills(" ".join(authors_publications_dict[aid]))  # skill names
+        skills = [str(skills_dict[skill]) for skill in expert_skills]  # skill ids
+        if len(skills) > 1:  # expert with at least two skills
+            authors_skills_dict[aid] = skills
+            open("../dblp-2015/" + t_name + "-authors-skills.txt", "a").write(str(aid) + "\t" + ",".join(skills) + "\n")
+    open("../dblp-2015/" + t_name + "-authors-pair.txt", "w").close()
+    for pair in collaborations_dict:
+        if len(collaborations_dict[pair]) > 1:  # authors pair with at least two collaborations
+            open("../dblp-2015/" + t_name + "-authors-pair.txt", "a").write(
+                str(pair) + "\t" + str(len(collaborations_dict[pair])) + "\n")
+    import networkx as nx
+    graph = nx.Graph()
+    for node in authors_dict:
+        if node in authors_skills_dict:
+            graph.add_node(node, nname=authors_dict[node], skills=",".join(authors_skills_dict[node]))
+        else:
+            graph.add_node(node, nname=authors_dict[node], skills="")
+    for pair in collaborations_dict:
+        denominator = len(authors_publications_dict[int(pair.split(":")[0])]) + len(
+            authors_publications_dict[int(pair.split(":")[1])]) - len(collaborations_dict[pair])
+        jd = 1 - (len(collaborations_dict[pair]) / denominator)
+        graph.add_edge(int(pair.split(":")[0]), int(pair.split(":")[1]), weight=jd)
+    largest_cc = nx.subgraph(graph, max(nx.connected_components(graph), key=len)).copy()
+    nx.write_gml(largest_cc, "../dblp-2015/j" + t_name + ".gml")
+
+
 def multiprocessing_func(l_txt):
-    make_reocrds_file(l_txt)
+    make_community_reocrds_file(l_txt)
 
 
 if __name__ == '__main__':
     starttime = time.time()
+    # make_reocrds_file("dblp")
     processes = []
-    for txt in ["dblp"]:
+    for txt in ["icdt"]:
         p = multiprocessing.Process(target=multiprocessing_func, args=(txt,))
         processes.append(p)
         p.start()
---
+++ utilities.py
@@ -0,0 +1,186 @@
+def get_skills(publication) -> list:
+    """
+    returns non trivial words of publication as skills packed in set
+    used to get skills of an expert
+    non trivial keywords that appear at least twice in his/her publications are skills
+    :param publication:
+    :return:
+    """
+    from nltk import word_tokenize
+    import re
+    from nltk.corpus import stopwords
+    all_words = word_tokenize(re.sub(r'[^a-zA-Z]', ' ', publication))
+    filtered_words = list()
+    skills = set()
+    for word in all_words:
+        if word not in stopwords.words('english') and len(word) > 2:
+            filtered_words.append(word.lower())
+    local_dict = list_to_freq(filtered_words)
+    for word, freq in local_dict.items():
+        if freq > 1:  # check non trivial words that appear at least twice
+            skills.add(word.lower())
+    lst = list(skills)
+    return sorted(lst)
+
+
+def list_to_freq(wordlist) -> dict:
+    """
+    return dictionary generated from given list
+    :param wordlist:
+    :return:
+    """
+    word_freq = dict()
+    for word in wordlist:
+        if word not in word_freq:
+            word_freq[word] = 1
+        else:
+            word_freq[word] += 1
+    return word_freq
+
+
+def get_skill_experts_dict(l_graph) -> dict:
+    """
+    return skill expert community dictionary for input l_graph
+    :param l_graph:
+    :return dict:
+    """
+    skill_experts = dict()
+    for node in l_graph.nodes():
+        if len(l_graph.nodes[node]) > 0:
+            for skill in l_graph.nodes[node]["skills"].split(","):
+                if skill in skill_experts:
+                    skill_experts[skill].append(node)
+                elif skill not in skill_experts:
+                    skill_experts[skill] = list([node])
+                else:
+                    pass
+    return skill_experts
+
+
+def get_task(publication) -> list:
+    """
+    return list of skills(id) of task of parameter
+    used to get non-trivial words of parameter(publication) that matched with skills
+    :param publication:
+    :return:
+    """
+    from nltk import word_tokenize
+    import re
+    from nltk.corpus import stopwords
+    all_words = word_tokenize(re.sub(r'[^a-zA-Z]', ' ', publication))
+    filtered_words = list()
+    for word in all_words:
+        if word not in stopwords.words('english') and len(word) > 2:
+            filtered_words.append(word.lower())
+    local_dict = list_to_freq(filtered_words)
+    print(filtered_words)
+    skills_dict = dict()
+    with open("../dblp-2020/vldb-skills.txt", "r") as file:
+        for line in file:
+            words = line.strip("\n").split()
+            skills_dict[words[1]] = words[0]
+    task_str = set(local_dict.keys()).intersection(set(skills_dict.keys()))
+    task = list()
+    for wrd in task_str:
+        task.append(skills_dict[wrd])
+        print(wrd + "\t" + skills_dict[wrd])
+    return list(task)  # returns list of id's of skills
+
+
+def generate_community_tasks(community, tasks) -> None:
+    """
+    This method called once, generates community tasks 1700 and 17
+    :return:
+    """
+    import os.path
+    import glob
+    import random
+    import networkx as nx
+    max_no = 0
+
+    file_list = glob.glob("../dblp-2020/" + community + "-" + str(tasks) + "-*.txt")
+    if len(file_list) >= 5:
+        print("please delete existing(old) files")
+    elif len(file_list) > 0 :
+        max_no = len(file_list)
+        file_path = "../dblp-2020/" + community + "-" + str(tasks) + "-tasks-" + str(max_no) + ".txt"
+    else:
+        file_path = "../dblp-2020/" + community + "-" + str(tasks) + "-tasks-" + str(max_no) + ".txt"
+    graph = nx.read_gml("../dblp-2020/" + community + ".gml")
+    community_skills = get_community_skills(graph)
+    all_tasks = list()
+    open(file_path, "w").close()
+    if tasks == 17:
+        for skills_count in range(4, 21):
+            task = set()
+            while len(task) < skills_count:
+                task.add(random.choice(list(community_skills)))
+                if len(task) == skills_count:
+                    all_tasks.append(list(task))
+            for task in all_tasks:
+                for skill in task:
+                    open(file_path, "a").write(skill + "\t")
+                open(file_path, "a").write("\n")
+            all_tasks.clear()
+    else:
+        for skills_count in range(4, 21):
+            for iter_count in range(1, 101):
+                task = set()
+                while len(task) < skills_count:
+                    task.add(random.choice(list(community_skills)))
+                    if len(task) == skills_count:
+                        all_tasks.append(list(task))
+                for task in all_tasks:
+                    for skill in task:
+                        open(file_path, "a").write(skill + "\t")
+                    open(file_path, "a").write("\n")
+                all_tasks.clear()
+
+
+def get_community_skills(graph) -> set:
+    """
+    skill set of community is returned
+    :param graph:
+    :return set:
+    """
+    community_skills = set()
+    for node in graph.nodes():
+        if len(graph.nodes[node]) > 0:
+            for skill in graph.nodes[node]["skills"].split(","):
+                if len(skill) > 0:
+                    community_skills.add(skill)
+    return community_skills
+
+#Ref: https://stackoverflow.com/questions/18393842/k-th-order-neighbors-in-graph-python-networkx
+def knbrs(G, start, k):
+    nbrs = set([start])
+    for l in range(k):
+        nbrs = set((nbr for n in nbrs for nbr in G[n]))
+    return nbrs
+
+#Ref: https://thispointer.com/python-check-if-any-string-is-empty-in-a-list/
+def is_empty_or_blank(msg):
+    """ This function checks if given string is empty
+     or contain only shite spaces"""
+    import re
+    return re.search("^\s*$", msg)
+
+#Ref: https://stackoverflow.com/questions/18393842/k-th-order-neighbors-in-graph-python-networkx
+def knbrcover(l_graph, start, k):
+    nbrs = knbrs(l_graph, start, k)
+    dnbrs = nbrs.copy()
+    hopskillcover = set()
+    for n in dnbrs:
+        skls = list(filter(None, l_graph.nodes[n]["skills"].split(",")))
+        if len(skls) == 0:
+            nbrs.remove(n)
+        else:
+            hopskillcover.update(skls)
+    return hopskillcover
+
+def get_expert_skills_dict(graph):
+    expert_skills = dict()
+    for node in graph.nodes():
+        if len(graph.nodes[node]) > 0:
+            expert_skills[node] = graph.nodes[node]["skills"].split(",")
+    return expert_skills
