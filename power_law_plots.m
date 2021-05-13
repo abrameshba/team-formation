bd99e8d6b8dc3e7669d1341df2826e5a0c85e463
Ramesh <abrameshba@gmail.com>
2021-05-14 02:15:22 +0530

    updated graphs

--- power_law_plots.m
+++ power_law_plots.m
@@ -16,37 +16,38 @@ plot  [STATS_min_x:STATS_max_x][STATS_min_y:STATS_max_y]  '../dblp-'.year.'/'.ne
         '../dblp-'.year.'/'.network.'-lc.txt' using 1:2 with point pointtype 3 lc rgb "#FF0000" title "low collaborating experts"
 #        f(x)  title "power law" lt 2 lw 1
 reset
-#set title "Expert skills histogram"
+#set title "Skills of an Expert histogram"
+stats '../dblp-'.year.'/'.network.'-expt-freq.txt' u 1:2
+set xrange [STATS_min_x/2:STATS_max_x*2]
+set yrange [STATS_min_y/2:STATS_max_y*2]
 set xlabel "No of skills of an expert"
 set ylabel "No of experts"
 set logscale xy
-set terminal postscript eps enhanced color
+set terminal postscript eps enhanced color  font "sans,14"
 set output network.'-'.year.'-expt-skl-pl.eps'
 a=1000
 b=-0.10
-stats '../dblp-'.year.'/'.network.'-expt-freq.txt' u 1:2
 fit f(x) '../dblp-'.year.'/'.network.'-expt-freq.txt' via a, b
-plot   [STATS_min_x:STATS_max_x][STATS_min_y:STATS_max_y] '../dblp-'.year.'/'.network.'-expt-freq.txt' using 1:2 with point pointtype 2 lc rgb "#FF0000" title "number of experts"
+plot   '../dblp-'.year.'/'.network.'-expt-freq.txt' using 1:2 with point pointtype 3  pointsize 2 lc rgb "#FF0000" title "number of experts"
 #        f(x)  title "power law" lt 2 lw 1
 reset
-#set title "experts for a skill histogram"
-set size 0.75, 0.75
-set xlabel "No of experts for a skill"
-set ylabel "No of skills"
-set label "common skills" at 1000,2
-set label "rare skills" at 5,40
+set title "Experts for a skill histogram"
+stats '../dblp-'.year.'/'.network.'-skl-freq.txt' u 1:2
+set xrange [STATS_min_x/2:STATS_max_x*2]
+set yrange [STATS_min_y/2:STATS_max_y*2]
+set xlabel "Number of experts for a skill : rare skills -> abundant skills"
+set ylabel "Number of skills"
 set logscale xy
 set terminal postscript eps enhanced color  font "sans,14"
 set output network.'-'.year.'-skl-expt-pl.eps'
 a=1000
 b=-0.10
-stats '../dblp-'.year.'/'.network.'-skl-freq.txt' u 1:2
 set boxwidth 0.9 relative
 set style data histograms
 set style histogram cluster
 set style fill solid 0.5 border lt -1
 fit f(x) '../dblp-'.year.'/'.network.'-skl-freq.txt' via a, b
-#plot   [STATS_min_x:STATS_max_x][STATS_min_y:STATS_max_y] '../dblp-'.year.'/'.network.'-skl-freq.txt' using 1:2 with point pointtype 2 lc rgb "#FF0000" title "number of skills"
-plot   [STATS_min_x:STATS_max_x][STATS_min_y:STATS_max_y] '../dblp-'.year.'/'.network.'-skl-freq.txt' using 1:2 smooth freq with boxes title "number of skills"
+#plot   [STATS_min_x:STATS_max_x][STATS_min_y:STATS_max_y] '../dblp-'.year.'/'.network.'-skl-freq.txt' using 1:2 with point pointtype 2 pointsize 3 lc rgb "#FF0000" title "number of skills"
+plot   '../dblp-'.year.'/'.network.'-skl-freq.txt' using 1:2 smooth freq with boxes title "number of skills"
 #        f(x)  title "power law" lt 2 lw 1
 }
